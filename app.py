acd062c2a9b933b1c1c48a23e92361a03fefe730
Ashley <tanaka.mush@gmail.com>
2024-05-06 16:52:14 +0100

    Title: Implemented Movie Rating Web Application with Flask and SQLAlchemy
    Body:
    
    1. Created a Flask application in `app.py` with routes for adding and rating movies. The `/add_movie` route uses the Movie DB API to search for movies by title and add them to the database. The `/rate_movie` route allows users to rate a movie and add a review.
    
    2. Defined SQLAlchemy models in `models.py` for the `Movie` and `User` entities. The `Movie` model has fields for title, year, description, rating, ranking, review, and img_url. The `User` model has fields for name, email, and password.
    
    3. Created WTForms forms in `forms.py` for adding and rating movies. The `FindMovieForm` has a field for the movie title, and the `RateMovieForm` has fields for the rating and review.
    
    4. Created Jinja2 templates for the different pages of the application. The `add.html` template displays the `FindMovieForm`, the `select.html` template displays a list of search results from the Movie DB API, and the `rate.html` template displays the `RateMovieForm`.
    
    5. Handled errors such as `KeyError` and `IntegrityError` in the Flask routes. Added error handling for cases where the Movie DB API does not return expected data or the user tries to insert invalid data into the database.
    
    6. Used the `requests` library to make HTTP requests to the Movie DB API. Parsed the JSON response from the API to extract movie data.
    
    7. Used the `flask_sqlalchemy` library to interact with a SQLite database. Created the database and tables with `db.create_all()`, and added and retrieved data with the `db.session` object.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
@@ -8,12 +10,12 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 from models import db, Movie
-from forms import RateMovieForm
+from forms import RateMovieForm, FindMovieForm
 
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_APP_KEY')
 Bootstrap5(app)
 
 # -----------------Configure DB-------------------------
@@ -21,19 +23,63 @@ app.config['SQLALCHEMY_DATABASE_URI']= "sqlite:///movies-project.db"
 db.init_app(app)
 
 
+# with app.app_context():
+#     db.create_all()
+
+MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
+MOVIE_DB_BEARER_TOKEN = os.environ.get("MOVIE_DB_API_KEY")
+API_KEY = os.environ.get("API_KEY")
+
 
 
 
 @app.route("/")
 def home():
+    result = db.session.execute(db.select(Movie).order_by(Movie.rating))
+    all_movies = result.scalars().all() # convert ScalarResult to Python List
 
+    for i in range(len(all_movies)):
+        all_movies[i].ranking = len(all_movies) - i
+    db.session.commit()
 
-    all_movies = Movie.query.all()
-
-    print(all_movies)
-    for movie in all_movies:
-        print(movie.title)
     return render_template("index.html", movies=all_movies)
+
+# New Add Route
+@app.route("/add_movie", methods=["GET", "POST"])
+def add_movie():
+    form = FindMovieForm()
+
+    if form.validate_on_submit():
+        movie_title = form.title.data
+        print(f"You entered {movie_title}")
+
+        #-----API config---
+        url = "https://api.themoviedb.org/3/search/movie"
+        headers = {
+            "accept": "application/json",
+            "Authorization": MOVIE_DB_BEARER_TOKEN
+        }
+        params = {
+            "query": movie_title,
+            "include_adult": True,
+            "language": "en-US",
+
+
+        }
+
+        response = requests.get(url, headers=headers, params=params)
+        data = response.json()
+
+        result_list = data.get("results")
+
+
+        print(data)
+
+
+        return render_template("select.html", options=result_list)
+
+
+    return render_template("add.html", form=form)
 
 # Adding the Update functionality / Patch Request
 @app.route("/edit", methods=['GET', 'POST'])
@@ -48,6 +94,59 @@ def rate_movie():
         db.session.commit()
         return redirect(url_for('home'))
     return render_template("edit.html", movie=movie, form=form)
+
+
+# Adding the Delete functionality / Delete Request
+@app.route("/delete")
+def delete_movie():
+    movie_id = request.args.get("id")
+    movie = db.get_or_404(Movie, movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+# Fetch request
+@app.route("/find", methods=["GET", "POST"])
+def find_movie():
+
+    movie_api_id = int(request.args.get("id"))
+    if movie_api_id:
+        movie_DB_url = "https://api.themoviedb.org/3/movie"
+        movie_API_url = f"{movie_DB_url}/{movie_api_id}"
+
+        headers = {
+            "accept": "application/json",
+            "Authorization": f"{MOVIE_DB_BEARER_TOKEN}"
+        }
+
+
+        response = requests.get(movie_API_url, headers=headers)
+        data = response.json()
+        print(f"API Data: {data}")
+
+        poster_path =data["poster_path"]
+        BASE_URL_IMG = 'https://image.tmdb.org/t/p/w500/' + poster_path
+
+        print(data["original_title"], data["release_date"][:4], data["overview"], BASE_URL_IMG)
+
+        new_movie = Movie(
+            title=data["original_title"],
+            year=int(data["release_date"][:4]),
+            description= data["overview"],
+            img_url= BASE_URL_IMG
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+
+        print(new_movie.id)
+
+
+        return redirect(url_for("rate_movie", id=new_movie.id))
+    return render_template("add.html")
+
+
+
 
 
 
--- forms.py
+++ forms.py
@@ -6,3 +6,7 @@ class RateMovieForm(FlaskForm):
     rating = StringField(label="Your Rating Out of 10 e.g 7.5", validators=[DataRequired()])
     review = TextAreaField(label="Your Review", validators=[DataRequired()])
     submit = SubmitField(label="Submit")
+
+class FindMovieForm(FlaskForm):
+    title = StringField(label="Movie Title",validators=[DataRequired(message="Field entry Required")])
+    submit = SubmitField(label="Add Movie")
